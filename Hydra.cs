5777375cf8138d2da4ccd37bfba7780964e7aa09
ManneLite <manne@mosslightstudio.com>
2023-09-26 11:54:06 +0200

    removed complex hydra logic

--- Hydra.cs
+++ Hydra.cs
@@ -4,8 +4,7 @@ public class Hydra : IDamageable, IAttacker
 {
     public int HP = 900;
     public NPCType Type = NPCType.Hostile;
-    private int _baseDamage = 10;
-    private int _heads = 7;
+    private int _heads = 3;
 
     public void Damage(int amount)
     {
@@ -14,14 +13,6 @@ public class Hydra : IDamageable, IAttacker
 
     public void Attack(IDamageable enemy)
     {
-        Random random = new Random();
-        for (int i = 0; i < _heads; i++)
-        {
-            int rDamage = random.Next(-2, 5);
-            int damage = _baseDamage + rDamage;
-            Console.WriteLine("Damage - " + "base: " + _baseDamage + " acid: " + rDamage);
-            Console.WriteLine("Hydra Head " + (i + 1) + " Dealt " + damage + " Damage to Player");
-            enemy.Damage(damage);
-        }
+        enemy.Damage(_heads * 10);
     }
 }
